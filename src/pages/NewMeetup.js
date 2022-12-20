986bc9ae1c7d4c7dc635efb991b18564f1e0b261
navid.asadipur <navid.asadipur@gmail.com>
2022-12-20 22:18:28 +0330

    add new meetup form and post data to firebase api

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,22 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage(){
+    function addMeetupHandler(meetupData){
+        fetch('https://reactcoursemeetup-default-rtdb.firebaseio.com/meetups.json',
+        {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+
     return (
-        <div>New Meetup page</div>
+        <section>
+        <h1>Add New Meetup</h1>
+        <NewMeetupForm onAddMeetup={addMeetupHandler}></NewMeetupForm>
+        </section>
     );
 }
 
