fd2ed674046b28a5eef47395ba1c73cfa47b760d
navid.asadipur <navid.asadipur@gmail.com>
2022-12-20 22:16:42 +0330

    add layout

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation"
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-    <MainNavigation></MainNavigation>
-      <Routes>
-        <Route path="/" element={<AllMeetupsPage />}>
-        </Route>
-        <Route path="/new-meetup" element={<NewMeetupPage />}>
-        </Route>
-        <Route path="/favorites" element={<FavoritesPage />}>
-        </Route>
-      </Routes>
-    </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<AllMeetupsPage />}></Route>
+          <Route path="/new-meetup" element={<NewMeetupPage />}></Route>
+          <Route path="/favorites" element={<FavoritesPage />}></Route>
+        </Routes>
+      </Layout>
   );
 }
 
