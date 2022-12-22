613a35bc7377d2a3c120ae23f13da2df7adbba6d
navid.asadipur <navid.asadipur@gmail.com>
2022-12-23 00:40:25 +0330

    add items to favorite page and favorite items count badge

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,6 +1,23 @@
+import { useContext } from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+
+import FavoritesContext from '../store/context-favorites';
+
 function FavoritesPage(){
+    const FavoritesCtx = useContext(FavoritesContext);
+
+    let content;
+    if (FavoritesCtx.totalFavorites === 0) {
+        content = <p>You got no favorites yet. Start Adding some?</p>
+    } else {
+        content = <MeetupList meetups={FavoritesCtx.favorites}/>
+    }
+
     return (
-        <div>Favorites page</div>
+        <section>
+            <h1>My Favorites</h1>
+            {content}
+        </section>
     );
 }
 
