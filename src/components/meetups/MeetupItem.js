5b51a7798adcd51a3e8fee262b54e7615f33888e
navid.asadipur <navid.asadipur@gmail.com>
2022-12-22 22:35:31 +0330

    using FavoriteCtx and add toggleFavoritesStatusHandler in MeetupItem.js

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,24 +1,47 @@
-import Card from '../ui/Card';
-import classes from "./MeetupItem.module.css"
+import { useContext } from "react";
+
+import Card from "../ui/Card";
+import classes from "./MeetupItem.module.css";
+import FavoritesContext from "../../store/context-favorites";
 
 function MeetupItem(props) {
+  const FavoritesCtx = useContext(FavoritesContext);
+
+  const itemIsFavorite = FavoritesCtx.itemIsFavorite(props.id);
+
+  function toggleFavoritesStatusHandler() {
+    if (itemIsFavorite) {
+      FavoritesCtx.removeFavorite(props.id);
+    } else {
+      FavoritesCtx.addFavorite({
+        id: props.id,
+        title: props.title,
+        description: props.description,
+        image: props.image,
+        address: props.address,
+      });
+    }
+  }
+
   return (
-  <li className={classes.item}>
-  <Card>
-    <div className={classes.image}>
-        <img src={props.image} alt={props.title} />
-    </div>
-    <div className={classes.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-    </div>
-    <div className={classes.actions}>
-        <button>To Favorites</button>
-    </div>
-    </Card>
-  </li>
-    );
+    <li className={classes.item}>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={toggleFavoritesStatusHandler}>
+            {itemIsFavorite ? "Remove from Favorites" : "To Favorites"}
+          </button>
+        </div>
+      </Card>
+    </li>
+  );
 }
 
 export default MeetupItem;
